0abe2ee207652d970bc8c63e6e4bd868f6bd591f
Sebastien <sebastien.z.johnson@gmail.com>
2026-08-03 21:34:32 -0400

    Started building out car metrics

---
+++ specs.go
@@ -0,0 +1,108 @@
+package main
+
+type carStatCore struct {
+	sizeWieght dimensions
+	engine powerplant
+	grip suspension
+	aero aerodynamics
+	gear drivetrain
+	specs performance
+}
+
+type performance struct {
+	mechTopSpeed struct
+	aeroTopSpeed struct
+	COG struct
+	rollCenter struct
+}
+
+type rulesets struct {
+	STL carStatCore
+}
+
+type checkRules struct {
+	myBuild CarStatCore
+	myClass rulesets
+}
+
+type dimensions struct {
+	weight float32
+	length float32
+	height float32
+	width float32
+	fTrack float32
+	rTrack float32
+	weightDistro float32
+	fAxle float32
+	rAxle float32
+}
+
+type powerplant struct {
+	displacement float32
+	bore float32
+	stroke float32
+	peakRPM float32
+	BHP horsePower
+	BTQ torque
+	WHP horsePower
+	WTQ torque
+	aspiration str
+}
+
+type horsePower struct {
+	pwr float32
+	RPM int
+}
+
+type torque struct {
+	tq float32
+	RPM int
+}
+
+type suspension struct {
+	fStyle str
+	rStyle str
+	fWheels wheel
+	fTires tire
+	rWheels wheel
+	rTires tire
+	ttsa float32
+}
+
+type wheel struct {
+	width float32
+	diameter float32
+}
+
+type tire struct {
+	width int
+	innerDia float32
+	outterDia float32
+}
+
+type aerodynamics struct {
+	coDrag float32
+	frontalArea float32
+	cda float32
+	fLift float32
+	rLift float32
+}
+
+
+type drivetrain struct {
+	gearset ratios
+	finalDrive float32
+}
+
+type ratios struct {
+	first float32
+	second float32
+	third float32
+	fourth float32
+	fifth float32
+	sixth float32
+	seventh float32
+	eighth float32
+	ninth float32
+	tenth float32
+}
